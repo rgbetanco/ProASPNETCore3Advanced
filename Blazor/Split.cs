739ba7fa7d896bc252f43dfdc55f9299df52f2a0
rgbetanco <rgbetanco@hotmail.com>
2020-12-22 15:13:34 +0800

    Add project files.

---
+++ Blazor/Split.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Advanced.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Advanced.Blazor
+{
+    public partial class Split
+    {
+        [Inject]
+        public DataContext dataContext { get; set; }
+        public IEnumerable<string> Names => dataContext.People.Select(p=>p.Firstname);
+    }
+}
---
+++ Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advanced.Models
+{
+    public class Department
+    {
+        public long DepartmentId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Person> People { get; set; }
+    }
+}
